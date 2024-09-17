861f7d2635c4c66864ae508ffc485b8bf2c272b2
Arnaud Rocher <arnaud.roche3@gmail.com>
2024-09-17 15:37:15 +0200

    Change WezTerm theme and maximize strategy

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,12 +2,26 @@ local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
-wezterm.on('gui-startup', function (cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():maximize()
+-- Thanks to https://github.com/wez/wezterm/issues/3173#issuecomment-1722531883
+wezterm.on('window-config-reloaded', function(window, pane)
+    -- approximately identify this gui window, by using the associated mux id
+    local id = tostring(window:window_id())
+
+    -- maintain a mapping of windows that we have previously seen before in this event handler
+    local seen = wezterm.GLOBAL.seen_windows or {}
+    -- set a flag if we haven't seen this window before
+    local is_new_window = not seen[id]
+    -- and update the mapping
+    seen[id] = true
+    wezterm.GLOBAL.seen_windows = seen
+
+    -- now act upon the flag
+    if is_new_window then
+        window:maximize()
+    end
 end)
 
-config.color_scheme = 'Monokai Dark (Gogh)'
+config.color_scheme = 'Monokai (dark) (terminal.sexy)'
 
 config.font = wezterm.font 'Hack Nerd Font'
 
