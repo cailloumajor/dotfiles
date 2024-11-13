57694276fe188ae17c90e8f6e568c723de8cab9e
Arnaud Rocher <arnaud.roche3@gmail.com>
2024-11-13 19:38:03 +0100

    Add a keybinding to copy with ANSI

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -27,4 +27,15 @@ config.font = wezterm.font "Hack Nerd Font"
 
 config.hide_tab_bar_if_only_one_tab = true
 
+config.keys = {
+    {
+        key = "E",
+        mods = "CTRL",
+        action = wezterm.action_callback(function(window, pane)
+            local ansi = window:get_selection_escapes_for_pane(pane)
+            window:copy_to_clipboard(ansi)
+        end),
+    },
+}
+
 return config
