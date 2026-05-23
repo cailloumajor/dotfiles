ae12c0d10dbdcd374924bcbf6d388a532b067d00
Arnaud Rocher <arnaud.roche3@gmail.com>
2026-05-23 15:22:22 +0200

    Add a function to start development environment

--- home/dot_oh-my-zsh-custom/plugins/zellij/zellij.plugin.zsh
+++ home/dot_oh-my-zsh-custom/plugins/zellij/zellij.plugin.zsh
@@ -2,12 +2,21 @@ if (( ! $+commands[zellij] )); then
     return
 fi
 
+function ide {
+    dir=$(realpath "$PWD")
+    base=$(basename "$dir" | tr '[:upper:]' '[:lower:]' | sed -E 's/[^a-z0-9]+/-/g; s/^-+|-+$//g')
+    hash=$(printf '%s' "$dir" | sha1sum | cut -c1-6)
+    session="${base}-${hash}"
+
+    zellij --layout dev attach --create "$session"
+}
+
 # If the completion file doesn't exist yet, we need to autoload it and
 # bind it to `zellij`. Otherwise, compinit will have already done that.
 if [[ ! -f "$ZSH_CACHE_DIR/completions/_zellij" ]]; then
-  typeset -g -A _comps
-  autoload -Uz _zellij
-  _comps[zellij]=_zellij
+    typeset -g -A _comps
+    autoload -Uz _zellij
+    _comps[zellij]=_zellij
 fi
 
 # Generate and load zellij completion
