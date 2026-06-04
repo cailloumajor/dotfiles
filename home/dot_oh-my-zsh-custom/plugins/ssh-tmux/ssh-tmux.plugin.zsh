500c451e4b7fa51196d7a5a1611e505d9e85d937
Arnaud Rocher <arnaud.roche3@gmail.com>
2026-06-04 11:28:00 +0200

    Enable remote tmux session reusing

--- home/dot_oh-my-zsh-custom/plugins/ssh-tmux/ssh-tmux.plugin.zsh
+++ home/dot_oh-my-zsh-custom/plugins/ssh-tmux/ssh-tmux.plugin.zsh
@@ -2,5 +2,5 @@ function ssh-tmux {
     local remote_host=$1
     local session_name=$2
 
-    ssh -t "$remote_host" "tmux -u new-session -s $session_name \; set -g status off"
+    ssh -t "$remote_host" "tmux -u new-session -A -s $session_name \; set -g status off"
 }
